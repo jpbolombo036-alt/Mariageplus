622a3e354a231349f02929d365d939882174c2b8
jpbolombo036-alt <jpbolombo036@gmail.com>
2026-08-31 13:51:39 +0100

    Ajoute de response

--- src/main/java/com/mariageplus/security/UserPrincipal.java
+++ src/main/java/com/mariageplus/security/UserPrincipal.java
@@ -2,6 +2,7 @@ package com.mariageplus.security;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
